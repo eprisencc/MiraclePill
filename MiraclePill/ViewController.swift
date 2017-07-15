9baf39a785b082131605152cc3920fc309fd58f2
Jonathan L <eprisencc@gmail.com>
2017-07-15 16:52:26 -0400

    Initial commit.

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Jonathan L. on 7/15/17.
+//  Copyright © 2017 Jonathan L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
